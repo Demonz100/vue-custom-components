597b7d822dd8156612e3b58bb506355626205cba
Chhorvorn <moeunchhorvorn@mcnc.co.kr>
2024-05-08 08:19:51 +0700

    create select component

---
+++ src/layout/LayoutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink> | 
+        <RouterLink to="/about">About</RouterLink> | 
+        <RouterLink to="/table">Table</RouterLink> | 
+        <RouterLink to="/select">Select</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+
+</script>
